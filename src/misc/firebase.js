bdd2548ded589e2ff70bcc2e053a75174ec99304
Ayush-3012 <diveshsrivastava31@gmail.com>
2023-11-25 13:55:40 +0530

    add firebase and its config to manage databse, storage, deployment and hosting

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const config = {
+    apiKey: "AIzaSyDFgh48Dzyx2Tq1HDK328IKoscOwROwVc8",
+    authDomain: "chat-web-app-3012.firebaseapp.com",
+    projectId: "chat-web-app-3012",
+    storageBucket: "chat-web-app-3012.appspot.com",
+    messagingSenderId: "512645540629",
+    appId: "1:512645540629:web:f9744d887f6610d4c8971c"
+  };
+
+const app = firebase.initializeApp(config)
