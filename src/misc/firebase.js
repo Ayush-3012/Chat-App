d8126c4068e8bc102f988f78362e09b6a4fa8692
Ayush-3012 <diveshsrivastava31@gmail.com>
2023-11-25 19:03:55 +0530

    added signIn page and SignIn with google using firbase auth and database

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,12 +1,16 @@
-import firebase from 'firebase/app'
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const config = {
-    apiKey: "AIzaSyDFgh48Dzyx2Tq1HDK328IKoscOwROwVc8",
-    authDomain: "chat-web-app-3012.firebaseapp.com",
-    projectId: "chat-web-app-3012",
-    storageBucket: "chat-web-app-3012.appspot.com",
-    messagingSenderId: "512645540629",
-    appId: "1:512645540629:web:f9744d887f6610d4c8971c"
-  };
+  apiKey: "AIzaSyDFgh48Dzyx2Tq1HDK328IKoscOwROwVc8",
+  authDomain: "chat-web-app-3012.firebaseapp.com",
+  projectId: "chat-web-app-3012",
+  storageBucket: "chat-web-app-3012.appspot.com",
+  messagingSenderId: "512645540629",
+  appId: "1:512645540629:web:f9744d887f6610d4c8971c",
+};
 
-const app = firebase.initializeApp(config)
+const app = initializeApp(config);
+export const auth = getAuth(app);
+export const database = getDatabase(app);
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,47 @@
-
+import { Button, Col, Container, Grid, Panel, Row } from "rsuite";
+import { auth, database } from "../misc/firebase";
+import { GoogleAuthProvider, signInWithPopup, getAdditionalUserInfo } from "firebase/auth";
+import { ref, serverTimestamp, set} from "firebase/database";
 
 const SignIn = () => {
+  const onGoogleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider)
+      .then((result) => {
+        const { isNewUser } = getAdditionalUserInfo(result);
+        if (isNewUser) {
+          const userProfileRef = ref(database, `/profiles/${result.user.uid}`);
+          set(userProfileRef, {
+            name: result.user.displayName,
+            createdAt: serverTimestamp(),
+          });
+        }
+        console.log("scucess");
+      })
+      .catch((err) => console.log(err.message));
+  };
   return (
-    <div>this is SignIn</div>
-  )
-}
+    <Container className="">
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to Chat-App</h2>
+                <p>Progressive chat platform for neophytes</p>
+              </div>
 
-export default SignIn
+              <div className="mt-3">
+                <Button block appearance="primary" onClick={onGoogleSignIn}>
+                  Continue with Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
+};
+
+export default SignIn;
